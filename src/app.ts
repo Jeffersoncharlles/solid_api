eb376d7cb107a42d83c7c1ca007d9928a2d00dca
Jefferson <jefferson@macbook.tv>
2023-03-20 19:37:40 -0300

    chore: create fist route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prima } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (req, res) => {
+  const requesterBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, name, password } = requesterBodySchema.parse(req.body)
+
+  await prima.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return res.status(201).send()
+})
